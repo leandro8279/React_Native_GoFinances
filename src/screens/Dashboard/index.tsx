29af7f68f729709f8e3a434720ca007199fc1d9a
Leandro8279 <Leandro8279@gmail.com>
2022-05-08 14:30:30 -0300

    Utilizando Ícones

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,17 +1,29 @@
 import React from "react";
-import {View, Text} from "react-native";
+import { View, Text, Image } from "react-native";
+import { Feather } from "@expo/vector-icons";
+import { styles } from "./styles";
 
-import {Header} from "@components/Header";
-
-import {styles} from "./styles";
-
-export function Dashboard(){
+import { colors } from "@global/styles";
+export function Dashboard() {
 	return (
 		<View style={styles.container}>
-		<Header/>
-			<Text style={styles.title}>
-				Dashboard
-			</Text>
+			<View style={styles.header}>
+				<View style={styles.wrapper}>
+					<View style={styles.info}>
+						<Image
+							source={{
+								uri: "https://avatars.githubusercontent.com/u/49030804?v=4",
+							}}
+							style={styles.photo}
+						/>
+						<View style={styles.user}>
+							<Text style={styles.title}>Òla,</Text>
+							<Text style={styles.subTitle}>Leandro</Text>
+						</View>
+					</View>
+					<Feather name="power" size={24} color={colors.secondary} />
+				</View>
+			</View>
 		</View>
 	);
 }
--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -1,12 +1,46 @@
-import {StyleSheet} from "react-native";
-
+import { StyleSheet } from "react-native";
+import { RFPercentage, RFValue } from "react-native-responsive-fontsize";
+import { colors, fonts } from "@global/styles";
 export const styles = StyleSheet.create({
-	container:{
+	container: {
 		flex:1,
+		backgroundColor:colors.background,
 	},
-	title:{
-		fontSize:18,
-		fontWeight:"bold",
-		color:"red"
-	}
+	header: {
+		width: "100%",
+		height: RFPercentage(42),
+		backgroundColor: colors.primary,
+		justifyContent: "center",
+		alignItems: "center",
+		flexDirection: "row",
+	},
+	wrapper: {
+		width: "100%",
+		paddingHorizontal: 24,
+		flexDirection:"row",
+		justifyContent:"space-between",
+		alignItems:"center"
+	},
+	info: {
+		flexDirection: "row",
+		alignItems: "center",
+	},
+	photo: {
+		width: RFValue(48),
+		height: RFValue(48),
+		borderRadius: 10,
+	},
+	user: {
+		marginLeft: 17,
+	},
+	title: {
+		fontSize: RFValue(18),
+		color: colors.shape,
+		fontFamily: fonts.regular,
+	},
+	subTitle: {
+		fontSize: RFValue(18),
+		color: colors.shape,
+		fontFamily: fonts.bold,
+	},
 });
