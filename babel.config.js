eb247878b59a51051135d64bea78f242fb27a2fa
Leandro8279 <Leandro8279@gmail.com>
2022-05-07 21:50:22 -0300

    Configurando o typescript no projeto

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,32 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+     plugins: [
+      [
+        "module-resolver",
+        {
+         root:".",
+         extensions:[
+           ".js",
+           ".jsx",
+           ".tsx",
+           ".ts",
+           ".android.js",
+           ".android.tsx",
+           ".ios.js",
+           ".ios.tsx"
+         ],
+          alias: {
+            "@assets": "./src/assets",
+            "@components":"./src/components",
+            "@global":"./src/global",
+            "@services":"./src/services",
+            "@screens":"./src/screens",
+            "@navigation":"./src/navigation",
+            "@utils":"./src/utils",
+          },
+        },
+      ],
+    ],
   };
 };
